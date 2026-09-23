a08cf9fa7621739889233d772a68f04ce28073ec
Ahmed Mamdouh <amamdouhdeveloper@gmail.com>
2026-09-23 08:19:03 +0300

    Weather and math MCP servers.

---
+++ servers/weather_server.py
@@ -0,0 +1,14 @@
+from typing import List
+
+from mcp.server.fastmcp import FastMCP
+
+mcp = FastMCP("Weather")
+
+@mcp.tool()
+async def get_weather(location: str) -> str:
+    """Get weather for location."""
+
+    return "Hot as hell"
+
+if __name__ == "__main__":
+    mcp.run(transport="sse") # This is the sse transport, we use sse to communicate with the server, it's a server-to-client communication.
