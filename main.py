3f9bd3d86cf3de24c733b4b277b86cbfba8572ac
Ahmed Mamdouh <amamdouhdeveloper@gmail.com>
2026-09-23 13:01:36 +0300

    MCP client setup. Dependencies added.

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 import asyncio
-import os
 
 from dotenv import load_dotenv
+from langchain.agents import create_agent
 from langchain_core.messages import HumanMessage
+# load_mcp_tools function is responsible for taking the tools which are exposed through the MCP server 
+# and transform it into a Langchain tool so we can use them into the LangGraph/LangChain agent.
 from langchain_mcp_adapters.tools import load_mcp_tools
-from langchain_openai import ChatOpenAI
-from langgraph.prebuilt import create_react_agent
+# from langchain_openai import ChatOpenAI
+from langchain_ollama import ChatOllama
+# ClientSession is responsible for MCP server communication.
+# StdioServerParameters is a pydantic class which has the fields of commands and args which represent how to run the MCP server, 
+# so the client needs to know how to run the MCP server whether to run with Python, Nodejs, Docker and so on.
 from mcp import ClientSession, StdioServerParameters
+# stdio_client It's gonna communicate to the MCP server through the transport of the standard input/output.
 from mcp.client.stdio import stdio_client
 
-
 load_dotenv()
+
+llm = ChatOllama(temperature=0, model="qwen3:8b")
+
+stdio_server_params = StdioServerParameters(
+    command = "python", # It means that the MCP server(math_server.py) we intend to run is implemented in Python.
+    args = ["/Users/ahmedmamdouh/Desktop/ai-agents/udemy/mcp-crash-course/servers/math_server.py"], # The path to the MCP server we need to run.
+)
 
 async def main():
     print("Hello from mcp-crash-course!")
